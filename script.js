9aba1afc97b5a8f8cfd20b244f5515ae04ebe863
Erwynpro117 <182699749+Erwynpro117@users.noreply.github.com>
2025-06-30 11:04:34 -0600

    Esquema de color dark

---
+++ script.js
@@ -0,0 +1,14 @@
+/* Enlazar los elementos del HTML */
+
+const toggleButton = document.getElementById('toggle-button');
+const body = document.body;
+
+/* Creacion de una funcion para ejecutar un codigo */
+
+toggleButton.addEventListener('click', () => {
+    alert('¡Diste click!');
+} );
+
+toggleButton.addEventListener('click', () => {
+    body.classList
+} );
