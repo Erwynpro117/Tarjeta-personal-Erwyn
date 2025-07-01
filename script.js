bf900aac8a8598e09d74bcc9b2d5f52034b56904
Erwynpro117 <182699749+Erwynpro117@users.noreply.github.com>
2025-07-01 10:57:29 -0600

    Cambio dinamico de icon-theme y marca grafica

--- script.js
+++ script.js
@@ -2,13 +2,17 @@
 
 const toggleButton = document.getElementById('toggle-button');
 const body = document.body;
+const logo = document.getElementById('logo');
 
 /* Creacion de una funcion para ejecutar un codigo */
 
 toggleButton.addEventListener('click', () => {
-    alert('¡Diste click!');
+   body.classList.toggle('dark');
+
+/* Cambiamos el logo segun el tema */
+
+const isDark = body.classList.contains('dark');
+
+logo.src = isDark ? './assets/marca-grafica-dark.svg' : './assets/marca-grafica.svg';
 } );
 
-toggleButton.addEventListener('click', () => {
-    body.classList
-} );
